53d0a92d02bdec6e77057ee6db36f7b6807a4f6b
datCodeGirl <adeolamarsh@gmail.com>
2024-05-10 12:34:22 -0400

    create-NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import NavBar from './Components/NavBar'
+//import { useState } from 'react'
 
 function App() {
 
   return (
     <>
-    <h1 className='bg-red-400 py-4 rounded-xl'>Hello World</h1>
+    <NavBar />
+
+    {/* <h1 className='bg-red-400 py-4 rounded-xl'>Hello World</h1> */}
     </>
   )
 }
