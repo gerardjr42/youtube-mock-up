6e3a1568be12e8918a4b529eb3f4437d866135c2
gerardjr42 <ggarciajr12@gmail.com>
2024-05-11 21:41:05 -0400

    add: home page fetch functionality & style

--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -1,1 +1,26 @@
 const URL = import.meta.env.VITE_API_BASE_URL;
+
+export function latestShow() {
+  return fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=8&key=${URL}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Fetching API was not successful")
+    }
+    return response.json();
+  })
+}
+//Use this to get most new / popular videos:
+// https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=8&key=${URL}
+
+//Use this to get channel information for specific video
+//https://www.googleapis.com/youtube/v3/channels?key=${URL}&part=snippet&id=UCOhtMAg7xh8wv_wUHMgFc-Q
+
+export function getChannel(channelId) {
+  return fetch(`https://www.googleapis.com/youtube/v3/channels?key=${URL}&part=snippet&id=${channelId}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Fetching channel information was not successful")
+    }
+    return response.json();
+  })
+}
