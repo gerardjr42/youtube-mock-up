848c4c185fdf1a86002daf937199b93672c223a9
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-05-13 16:25:28 -0400

    update: NavBar and SearchBar styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import SearchBar from "./SearchBar"
 
 export default function NavBar () {
     return (
-        <div className=' NavBar bg-red-500 space-x-4 gap:4px px-3  text-white justify-start flex flex-auto' > 
-            <Link to="/"><p>YouTube</p></Link>
-            <Link to="/about"><p>About</p></Link>
+        <div className=' NavBar bg-red-500 space-x-4 gap:4px px-3  text-white  flex flex-auto h-10 '> 
+            <Link to="/"><img src="https://pbs.twimg.com/media/GNeiDwEXUAA23WW?format=jpg&name=900x900" className="m-1 w-16 rounded-md"/></Link>
+            <Link to="/about"><p className="m-2">About</p></Link>
             <SearchBar />
         </div>
 
