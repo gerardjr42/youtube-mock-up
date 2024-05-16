fc7e1ddcb15d81411416f6486e7edf74293b0be4
gerardjr42 <ggarciajr12@gmail.com>
2024-05-15 20:26:10 -0400

    fix:navbar and image profile path

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -33,7 +33,7 @@ export default function CommentForm() {
     <>
       <div className="flex flex-row">
         <img
-          src="/public/assets/images/profile-user.png"
+          src="/assets/images/profile-user.png"
           alt=""
           className="ml-6 mt-4 h-9 w-9 rounded-full bg-[#787878]"
         />
